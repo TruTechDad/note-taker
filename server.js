ec2ece3084a59ebeb8de3089fd04cd05a2186fa3
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2024-01-16 21:13:17 -0500

    added on to the server.js and moved the html's to public

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const notesFilePath = path.join(__dirname, 'db', 'db.json'); // Add this line
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,21 +22,22 @@ app.get('*', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+  const notes = JSON.parse(fs.readFileSync(notesFilePath, 'utf8'));
   res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
   const newNote = req.body;
   newNote.id = uuidv4();
-  
-  const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+
+  const notes = JSON.parse(fs.readFileSync(notesFilePath, 'utf8'));
   notes.push(newNote);
 
-  fs.writeFileSync('db/db.json', JSON.stringify(notes));
+  fs.writeFileSync(notesFilePath, JSON.stringify(notes));
 
   res.json(newNote);
 });
+
 
 
 app.listen(PORT, () => {
