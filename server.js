9e72e9dcdd44bf521a8877775e6cb12740d9366b
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2024-01-28 18:54:07 -0500

    added some changes

--- server.js
+++ server.js
@@ -6,19 +6,15 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const notesFilePath = path.join(__dirname, 'db', 'db.json'); // Add this line
+const notesFilePath = './db/db.json'; 
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname,  'views/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
@@ -33,12 +29,14 @@ app.post('/api/notes', (req, res) => {
   const notes = JSON.parse(fs.readFileSync(notesFilePath, 'utf8'));
   notes.push(newNote);
 
-  fs.writeFileSync(notesFilePath, JSON.stringify(notes));
+  fs.writeFileSync(notesFilePath, JSON.stringify(notes, null, 2));
 
-  res.json(newNote);
+  res.json('success');
 });
 
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
